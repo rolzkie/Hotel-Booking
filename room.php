21a7ad988e7b847acb85982d531b5aa7ea716c24
Kevin1117-23 <kevingelle5423@gmail.com>
2026-05-23 08:10:38 +0800

    Update image sources for room thumbnails

--- room.php
+++ room.php
@@ -157,7 +157,7 @@
           <div class="room__thumbs">
             <div class="room__thumb"><img src="assets/rooms/RoyalSuite/RoyalBedroom.jpeg" alt="option 1" /></div>
             <div class="room__thumb"><img src="assets/rooms/RoyalSuite/RoyalLivingRoom.jpeg" alt="option 2" /></div>
-            <div class="room__thumb"><img src="assets/rooms/RoyalSuite/RoyalDiningRoom.jpeg" alt="option 3" /></div>
+            <div class="room__thumb"><img src="assets/rooms/RoyalSuite/RoyalDiningArea.jpeg" alt="option 3" /></div>
           </div>
           <div class="room__card__details">
             <h4>Royal Suite</h4>
@@ -180,9 +180,9 @@
             </div>
           </div>
           <div class="room__thumbs">
-            <div class="room__thumb"><img src="assets/presidential(png)/winestorage.jpeg" alt="option 1" /></div>
-            <div class="room__thumb"><img src="assets/presidential(png)/MasterBathroom.jpeg" alt="option 2" /></div>
-            <div class="room__thumb"><img src="assets/presidential(png)/massageroom.jpeg" alt="option 3" /></div>
+            <div class="room__thumb"><img src="assets/rooms/presidential(png)/winestorage.jpeg" alt="option 1" /></div>
+            <div class="room__thumb"><img src="assets/rooms/presidential(png)/MasterBathroom.jpeg" alt="option 2" /></div>
+            <div class="room__thumb"><img src="assets/rooms/presidential(png)/massageroom.jpeg" alt="option 3" /></div>
           </div>
           <div class="room__card__details">
             <h4>Presidential Suite</h4>
